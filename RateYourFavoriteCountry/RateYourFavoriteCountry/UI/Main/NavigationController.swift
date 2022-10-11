c2e0fce605081692e10e29d43054ad3d8e50c88d
asiuzzaman <zaman1304006@gmail.com>
2022-10-12 02:35:12 +0600

    Add NavigationController file

---
+++ RateYourFavoriteCountry/RateYourFavoriteCountry/UI/Main/NavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  NavigationController.swift
+//  RateYourFavoriteCountry
+//
+//  Created by Md. Asiuzzaman on 12/10/22.
+//
+
+import UIKit
+
+final class NavigationController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //setupAppearance()
+    }
+}
