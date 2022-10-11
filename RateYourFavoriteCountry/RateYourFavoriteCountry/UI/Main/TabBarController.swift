ab76f03b3f6ddb90a93eecf7c02a2111843efd1d
asiuzzaman <zaman1304006@gmail.com>
2022-10-12 02:31:23 +0600

    Add TabBarController file

---
+++ RateYourFavoriteCountry/RateYourFavoriteCountry/UI/Main/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  RateYourFavoriteCountry
+//
+//  Created by Md. Asiuzzaman on 12/10/22.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //setupTabBar()
+        //setupViewControllers()
+    }
+}
