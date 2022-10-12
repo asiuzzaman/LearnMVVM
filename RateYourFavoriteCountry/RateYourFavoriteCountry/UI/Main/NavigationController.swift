39ea5fbbe402911e796d73a604ac058143f2131e
asiuzzaman <zaman1304006@gmail.com>
2022-10-13 00:32:21 +0600

    Setup tabbar controller and navigationController

--- RateYourFavoriteCountry/RateYourFavoriteCountry/UI/Main/NavigationController.swift
+++ RateYourFavoriteCountry/RateYourFavoriteCountry/UI/Main/NavigationController.swift
@@ -10,6 +10,22 @@ import UIKit
 final class NavigationController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        //setupAppearance()
+        setupAppearance()
+    }
+}
+
+private extension NavigationController {
+    func setupAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.titleTextAttributes = [
+            .foregroundColor: UIColor.black,
+                .font: UIFont.systemFont(ofSize: 20, weight: .bold)
+        ]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.black]
+        appearance.backgroundColor = UIColor.green // not exactly green have to check it.
+        navigationBar.standardAppearance = appearance
+        navigationBar.scrollEdgeAppearance = appearance
+        
     }
 }
--- RateYourFavoriteCountry/RateYourFavoriteCountry/UI/Main/TabBarController.swift
+++ RateYourFavoriteCountry/RateYourFavoriteCountry/UI/Main/TabBarController.swift
@@ -11,7 +11,36 @@ final class TabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //setupTabBar()
+        setupTabBar()
         //setupViewControllers()
+    }
+}
+
+private extension TabBarController {
+    
+    
+    func setupTabBar() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.green // make green
+
+        setTabBarItemAppearance(for: appearance.compactInlineLayoutAppearance)
+        setTabBarItemAppearance(for: appearance.stackedLayoutAppearance)
+        setTabBarItemAppearance(for: appearance.inlineLayoutAppearance)
+        tabBar.standardAppearance = appearance
+    }
+
+    func setTabBarItemAppearance(for appearance: UITabBarItemAppearance) {
+        appearance.normal.iconColor = .lightGray
+        appearance.normal.titleTextAttributes = [
+            .font: UIFont.systemFont(ofSize: 20),
+            .foregroundColor: UIColor.black
+        ]
+
+        appearance.selected.iconColor = .white
+        appearance.selected.titleTextAttributes = [
+            .font: UIFont.systemFont(ofSize: 20, weight: .bold),
+            .foregroundColor: UIColor.green // make green
+        ]
     }
 }
