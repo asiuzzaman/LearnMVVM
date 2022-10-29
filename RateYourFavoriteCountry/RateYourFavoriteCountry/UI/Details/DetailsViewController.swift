7fbde7cb1fb733d89e07d17f47ef70b370dd95e0
asiuzzaman <zaman1304006@gmail.com>
2022-10-30 01:02:30 +0600

    Add methods detailsViewModelDelegate, continue detailsViewController

--- RateYourFavoriteCountry/RateYourFavoriteCountry/UI/Details/DetailsViewController.swift
+++ RateYourFavoriteCountry/RateYourFavoriteCountry/UI/Details/DetailsViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    private var viewModel: DetailsViewModelDelegate!
+    
+    @IBOutlet weak var holderView: UIView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var iconCollectionView: UICollectionView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    static func makeViewController(icon: Icon, allowRating: Bool) -> DetailsViewController? {
+        let viewController = UIStoryboard(name: "Details", bundle: .main)
+            .instantiateViewController(withIdentifier: "DetailsViewController")
+
+        guard let detailsViewController = viewController as? DetailsViewController else {
+            return nil
+        }
+
+        detailsViewController.viewModel = DetailsViewModel(icon: icon, allowRating: allowRating)
+        return detailsViewController
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        iconCollectionView.reloadData()
     }
 
 }
--- RateYourFavoriteCountry/RateYourFavoriteCountry/UI/Details/DetailsViewModel.swift
+++ RateYourFavoriteCountry/RateYourFavoriteCountry/UI/Details/DetailsViewModel.swift
@@ -7,6 +7,18 @@
 
 import UIKit
 
+protocol DetailsViewModelDelegate: AnyObject {
+    var allowRating: Bool { get }
+    var title: String { get }
+    var ratingValue: Float { get }
+    func setRatingValue(to value: Int)
+
+    var iconImage: UIImage? { get }
+    var ratingLabel: String { get }
+
+    var isSavable: Bool { get }
+    func saveRating()
+}
 
 final class DetailsViewModel {
     let allowRating: Bool
@@ -22,5 +34,35 @@ final class DetailsViewModel {
         rating = iconManager.rating(icon: icon)
         initalRating = iconManager.rating(icon: icon)
         self.allowRating = allowRating
+    }
+}
+
+extension DetailsViewModel: DetailsViewModelDelegate {
+    var ratingValue: Float {
+        Float(rating.rawValue)
+    }
+    
+    func setRatingValue(to value: Int) {
+        rating = Icon.Rating(rawValue: value) ?? .veryBad
+    }
+    
+    var iconImage: UIImage? {
+        icon.image
+    }
+    
+    var ratingLabel: String {
+        ["Rating: ",rating.label].joined(separator: " ")
+    }
+    
+    var isSavable: Bool {
+        initalRating != rating
+    }
+    
+    func saveRating() {
+        iconManager.vote(icon: icon, rating: rating)
+    }
+    
+    var title: String {
+        return icon.name
     }
 }
