ef24764e566f565e96721e754bb71deb75e39ec5
asiuzzaman <zaman1304006@gmail.com>
2022-11-04 02:02:06 +0600

    Add RatingTableView and it's associated files

---
+++ RateYourFavoriteCountry/RateYourFavoriteCountry/UI/Rating/RatingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RatingViewModel.swift
+//  RateYourFavoriteCountry
+//
+//  Created by Md. Asiuzzaman on 30/10/22.
+//
+
+import Foundation
---
+++ RateYourFavoriteCountry/RateYourFavoriteCountry/UI/Rating/view/RatingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RatingTableViewCell.swift
+//  RateYourFavoriteCountry
+//
+//  Created by Md. Asiuzzaman on 4/11/22.
+//
+
+import UIKit
+
+class RatingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
